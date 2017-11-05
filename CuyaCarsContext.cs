1e8579e19a6c8f4a8077436726506a9a11292429
Isamartinrodriguez <33358446+Isamartinrodriguez@users.noreply.github.com>
2017-11-05 19:03:12 +0100

    Add files via upload

---
+++ CuyaCarsContext.cs
@@ -0,0 +1,19 @@
+﻿using CuyaCars.OBJ;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace CuyaCars.DA
+{
+    public class CuyaCarsContext : DbContext
+    {
+        public DbSet<Coche> Coches { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+
+    
+
+}
