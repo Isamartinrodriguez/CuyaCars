cef9240518e99f411daf7aed0fd22ba9d0bea8bb
Isamartinrodriguez <33358446+Isamartinrodriguez@users.noreply.github.com>
2017-11-05 13:35:23 +0100

    Add files via upload

---
+++ DACoches.cs
@@ -0,0 +1,30 @@
+﻿using CuyaCars.OBJ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CuyaCars.DA
+{
+    public class DACoches
+    {
+        CuyaCarsContext MiDBContext;
+
+        public DACoches()
+        {
+            MiDBContext = new CuyaCarsContext();
+        }
+
+
+        public void AgregarBDCoches(Coche Micoche )
+        {
+            using (var db = MiDBContext)
+            {
+                db.Coches.Add(Micoche);
+                db.SaveChanges();
+            }
+        }
+
+    }
+
+}
